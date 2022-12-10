e0c83281dd9802f662a5f353ce78c498a1694186
Oscar Lopez <57050336+keyle0015@users.noreply.github.com>
2022-12-10 03:33:53 -0700

    more files

--- js/insert.js
+++ js/insert.js
@@ -1,7 +1,18 @@
-$(function () {
-    $("#headerArea").load("shared/nav.html");
-    $("#footer").load("shared/footer.html");
-    $.get('shared/head.html', function (response) {
-        $('head').append(response);
+changePage("/contents/home.html");
+
+function changePage(page, style) {
+    $(function () {
+        $("#mainCont").load(page);
     });
-});
+    
+    var head = document.head;
+    var link = document.createElement("link");
+    
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = style;
+    
+    head.appendChild(link);
+    
+    console.log("BOO")
+}
